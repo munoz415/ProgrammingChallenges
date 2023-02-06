0cae92a566c3930d246348effe59ef6534ea7c3d
Mark Anthony Munoz <mamunoz415@gmail.com>
2023-02-05 21:55:26 -0600

    Unit Test Added

--- csv-combiner/csv-combiner.py
+++ csv-combiner/csv-combiner.py
@@ -1,23 +1,29 @@
 import sys
 import csv
+import filecmp
+import unittest
 
 col_names = []
+f_names = []
 def main():
     #ensure that there are at least 2 given csv files
     if(len(sys.argv) < 3):
         print("Incorrect number of arguments. \nMust be at least 2 csv files")
         sys.exit()
-    
-    #create the header for the new csv file
-    create_header()
-
-    #fill the new csv file with new contents
-    fill_file()
 
 
-def fill_file():
-    #input contents from given files
     for name in sys.argv[1:]:
+        f_names.append(name)
+    
+    #fill the new csv file with new contents
+    fill_file(f_names)
+
+
+def fill_file(file_names):
+    #create the header for the new csv file
+    create_header(file_names)
+    #input contents from given files
+    for name in file_names:
         #get current file name
         split_name = name.split("/")
         f_name = split_name[-1]
@@ -33,11 +39,14 @@ def fill_file():
                 #append the name of the current file
                 out_row.append(f_name)
                 #append row to new csv file
-                print(out_row)
+                with open('combine.csv', 'a') as csvfile:
+                    writer = csv.writer(csvfile, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL)
+                    writer.writerow(out_row)
+                    csvfile.close()
 
-def create_header():
+def create_header(file_names):
     #for each file input
-    for name in sys.argv[1:]:
+    for name in file_names:
         #get columns in file
         with open(name, newline='') as f:
             reader = csv.reader(f)
@@ -48,7 +57,10 @@ def create_header():
 
     #append a new column to header for the file name
     col_names.append("filename")
-    print(col_names)
+    with open('combine.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL)
+        writer.writerow(col_names)
+        csvfile.close()
 
 def add_header(curr_header):
     #for every name in the given header
@@ -58,6 +70,13 @@ def add_header(curr_header):
             #append name to header
             col_names.append(name)
 
+class TestCombine(unittest.TestCase):
+    def test_combine(self):
+        test_names = ['test1.csv', 'test2.csv']
+        fill_file(test_names)
+        self.assertTrue(filecmp.cmp('combine.csv', 'testRes.csv', shallow=False), "Files not equal.")
+
 
 if __name__ == '__main__':
     main()
+    #unittest.main()
